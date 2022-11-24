c924ebfaf1467eb61aebbda62dbc2de634b1f6a8
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-11-24 12:11:56 +0600

    configure auth context

---
+++ src/AuthContext/AuthContext.js
@@ -0,0 +1,71 @@
+import React, { createContext, useEffect, useState } from "react";
+import app from "../Firebase/Firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+export const AuthUserContext = createContext();
+const auth = getAuth(app);
+const AuthContext = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+
+  // Login with google
+
+  const googleLogin = () => {
+    setLoader(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const createAccount = (email, password) => {
+    setLoader(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUser = (profile) => {
+    setLoader(true);
+    return updateProfile(auth.currentUser, profile);
+  };
+
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setLoader(false);
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const authValue = {
+    user,
+    loader,
+    createAccount,
+    updateUser,
+    login,
+    logout,
+    googleLogin,
+  };
+  return (
+    <AuthUserContext.Provider value={authValue}>
+      {children}
+    </AuthUserContext.Provider>
+  );
+};
+
+export default AuthContext;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,4 +1,7 @@
 import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Login/Login";
+import Signup from "../Pages/Signup/Signup";
+import PageNotFound from "../Shared/PageNotFound/PageNotFound";
 
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Main } = require("../Layout/Main");
@@ -7,7 +10,22 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    children: [{ path: "/", element: <Home></Home> }],
+    children: [
+      { path: "/", element: <Home></Home> },
+
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
+      },
+      {
+        path: "*",
+        element: <PageNotFound></PageNotFound>,
+      },
+    ],
   },
 ]);
 
